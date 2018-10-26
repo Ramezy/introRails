e13697abdd7971766f2e0ce557a0784f61f632e7
Aramide <aramideakanbi@gmail.com>
2018-10-26 11:45:35 -0500

    Added the matches and team controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'about/index'
-  get 'season/index'
-  get 'season/show'
+  get 'teams/index'
+  get 'teams/show'
+  get 'matches/index'
+  get 'matches/show'
+  resources :about , only:[:index]
+  resources :seasons , only:[:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
