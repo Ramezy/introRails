8afffddfb0e606fdbda14c313592a61b10439690
Aramide <aramideakanbi@gmail.com>
2018-11-06 19:19:42 -0600

    created the first customer model

---
+++ db/migrate/20181107011911_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :address
+      t.boolean :isLogin
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_184502) do
+ActiveRecord::Schema.define(version: 2018_11_07_011911) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.boolean "isLogin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "matches", force: :cascade do |t|
     t.string "name"
